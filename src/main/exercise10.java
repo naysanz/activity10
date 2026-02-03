204772ad707a1388b0a59492976b6862229d8615
Maddi_L_M <mlaguna003@ikasle.ehu.eus>
2026-02-03 09:48:22 +0100

    new text added by Maddi

--- src/main/exercise10.java
+++ src/main/exercise10.java
@@ -1,3 +1,7 @@
 public class exercise10 {
-
+    @FXML
+    public void switchToSecondary() throws IOException {
+        App.setRoot("Secondary");
+        System.out.println("Maddi was here");
+    }
 }
